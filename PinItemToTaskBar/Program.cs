67fe7e50f5397b194187dba2decc46c26cf826ee
alex-konstantinov <alex.konstantinov@gmail.com>
2018-08-01 11:06:22 +0800

    Implementation.

--- PinItemToTaskBar/Program.cs
+++ PinItemToTaskBar/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,29 @@ namespace PinItemToTaskBar
 {
     class Program
     {
+        private static void PinUnpinTaskBar(string executablePath)
+        {
+            if (!File.Exists(executablePath)) throw new FileNotFoundException(executablePath);
+
+            var tempVbsFile = Path.GetTempFileName().Replace(".tmp", ".vbs");
+            File.WriteAllText(tempVbsFile, Resource.PinItem);
+
+            var processOptions = new ProcessStartInfo("cscript", $"/nologo /b {tempVbsFile} \"{executablePath}\"")
+            {
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            var process = Process.Start(processOptions);
+            process.WaitForExit();
+        }
+
         static void Main(string[] args)
         {
+            var assemblyPath = "c:\\windows\\";
+            var pinExecutable = "notepad.exe";
+
+            PinUnpinTaskBar(Path.Combine(Path.GetDirectoryName(assemblyPath), pinExecutable));
         }
     }
 }
